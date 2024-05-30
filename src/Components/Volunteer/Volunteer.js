38101d205354559c2e08fcbe951758b3f86850d5
Sharon-Anita-Omonge <sharonanita2021@gmail.com>
2024-05-30 11:58:44 +0300

    vol

--- src/Components/Volunteer/Volunteer.js
+++ src/Components/Volunteer/Volunteer.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Volunteer.css";
 import { Link } from "react-router-dom";
 import rightarrow from "../../assets/rightchev.png";
@@ -16,6 +15,45 @@ const Volunteer = () => {
 					</Link>
 					<img src={rightarrow} alt='right arrow' />
 					<h5>Volunteer</h5>
+				</div>
+			</div>
+			<div className='form-container container'>
+				<div className='form-section'>
+					<form
+						action='mailto:your-email@example.com'
+						method='POST'
+						enctype='text/plain'
+						className='form'
+					>
+						<label>
+							<input type='text' name='name' placeholder='Name' required />
+						</label>
+						<label>
+							<input type='email' name='email' placeholder='Email' required />
+						</label>
+						<label>
+							<textarea
+								name='message'
+								rows='5'
+								placeholder='Why do you want to be a volunteer?'
+								required
+							/>
+						</label>
+						<button type='submit' className='btn' id='butt'>
+							Become a Volunteer
+						</button>
+					</form>
+				</div>
+				<div className='text-section'>
+					<h4>Become a Volunteer</h4>
+					<h3>Let’s make a difference in the lives of others</h3>
+					<p>
+						Your efforts as a volunteer can create positive change, providing
+						mentorship, education, and support. Together, we can empower young
+						individuals, fostering growth, resilience, and hope. Join us today
+						and be part of something meaningful—a network of compassion,
+						inspiration, and transformation.
+					</p>
 				</div>
 			</div>
 		</div>
